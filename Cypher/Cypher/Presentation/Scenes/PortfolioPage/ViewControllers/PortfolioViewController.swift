20a6eb7c979e74a1a057f7d0e18188e86d22ef54
Nkhorbaladze <nikhorbaladze@tbcbank.com.ge>
2025-01-16 04:29:13 +0400

    Add portfolio view controller for empty state

--- Cypher/Cypher/Presentation/Scenes/PortfolioPage/ViewControllers/PortfolioViewController.swift
+++ Cypher/Cypher/Presentation/Scenes/PortfolioPage/ViewControllers/PortfolioViewController.swift
@@ -6,7 +6,41 @@
 //
 
 import UIKit
+import SwiftUI
 
 final class PortfolioViewController: UIViewController {
+    private lazy var emptyPortfolioView: UIHostingController<EmptyPortfolioView> = {
+        let hostingController = UIHostingController(
+            rootView: EmptyPortfolioView()
+        )
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        hostingController.view.backgroundColor = .clear
+        return hostingController
+    }()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        view.backgroundColor = UIColor(named: AppColors.backgroundColor.rawValue)
+        setupUI()
+        setupConstraints()
+    }
+    
+    private func setupUI() {
+        addChild(emptyPortfolioView)
+        view.addSubview(emptyPortfolioView.view)
+        emptyPortfolioView.didMove(toParent: self)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            emptyPortfolioView.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            emptyPortfolioView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            emptyPortfolioView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            emptyPortfolioView.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
